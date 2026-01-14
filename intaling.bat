1841a97e73ccf5b211dbe05d9fcd7ab490143bdf
thehaydenshow <lariell46755@gmail.com>
2026-01-14 15:21:37 -0500

    Add files via upload

--- intaling.bat
+++ intaling.bat
@@ -6,12 +6,4 @@ pause
 echo enter your product key
 echo it,s hayden-hayden-hayden-hayden
 set /p input=
-if /i  %onlymoms%==hayden-hayden-hayden-hayden goto setup[2]
-:setup[2]
-cls
-echo copying files
-timeout 10
-cls
-echo your pc will restart in 10 secs
-timeout 10
-C:\Users\theha\Desktop\windows-11-plus\setup/pc.bat
+if /i  %onlymoms%==hayden-hayden-hayden-hayden start C:\Users\theha\Desktop\windows-11-plus\pc.bat
