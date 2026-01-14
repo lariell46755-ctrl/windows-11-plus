b1e2b261537738c8d29f5058ac6701878d5b6a6e
thehaydenshow <lariell46755@gmail.com>
2026-01-14 13:45:21 -0500

    Add files via upload

---
+++ intaling.bat
@@ -0,0 +1,17 @@
+@echo off
+echo intalling from your pc
+timeout 10
+echo done
+pause
+echo enter your product key
+echo it,s hayden-hayden-hayden-hayden
+set /p input=
+if /i  %onlymoms%==Yes goto setup[2]
+:setup[2]
+cls
+echo copying files
+timeout 10
+cls
+echo your pc will restart in 10 secs
+timeout 10
+C:\Users\theha\Desktop\windows-11-plus\setup/pc.bat
---
+++ pc.bat
@@ -0,0 +1,4 @@
+md browser
+md my pc
+md settings
+shutdown -r -t 0
